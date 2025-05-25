3d5c9364a56f574ba665887dafb2d3fec419644a
Vinay Developer <developervsandhu@gmail.com>
2025-05-24 20:44:33 -0400

    Working Custom Backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,14 @@
 import client from "@repo/db/client";
+import cors from "cors";
 import express, { Express, Request, Response } from "express";
+
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
-  const user = client.user.findMany();
-  return res.json({ message: "up" });
+app.use(cors());
+
+app.get("/", async (req: Request, res: Response) => {
+  const user = await client.user.findMany();
+  return res.json({ message: "up", user });
 });
 
 app.listen(8080, () => console.log("up & running"));
